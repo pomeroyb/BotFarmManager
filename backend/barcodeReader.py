00a00edfeae273825db0bc2fb1e55464e7482100
Brandon Pomeroy <pomeroyb@uw.edu>
2015-11-03 14:17:27 -0800

    Refactor getInput

--- backend/barcodeReader.py
+++ backend/barcodeReader.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 hid = { 4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm', 17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y', 29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ', 45: '-', 46: '=', 47: '[', 48: ']', 49: '\\', 51: ';' , 52: '\'', 53: '~', 54: ',', 55: '.', 56: '/'  }
 
@@ -12,7 +13,11 @@ shift = False
 done = False
 
 while not done:
+    getInput();
+print ss
 
+
+def getInput()
    ## Get the character from the HID
    buffer = fp.read(8)
    for c in buffer:
@@ -48,6 +53,5 @@ while not done:
 
             ## if not a 2 then lookup the mapping
             else:
-               ss += hid[ int(ord(c)) ]
-         
-print ss
+               ss += hid[ int(ord(c)) ]    
+ 
