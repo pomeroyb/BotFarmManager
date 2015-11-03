6d0411666c4f9871aeb1808c7a56872915cc59ee
Brandon Pomeroy <pomeroyb@uw.edu>
2015-11-03 15:17:10 -0800

    Add comments

--- backend/apiTest.py
+++ backend/apiTest.py
@@ -1,15 +1,23 @@
 import barcodeReaderAPI
 
-ended = False
+# Create a BarcodeReader object
 reader = barcodeReaderAPI.BarcodeReader()
+
+# Some vars to decide when to end our program
+ended = False
 count = 0
 
 while not ended:
     if (count > 5):
         ended = True
+    # Update the reader's input
     reader.getInput()
+    
+    # Check if the reader has a full barcode
     if (reader.done):
+        # Display the reader's output
         print reader.output
+        # Make sure you clear the reader immediately after grabbing data from it.
         reader.clear()
         count += 1
     
