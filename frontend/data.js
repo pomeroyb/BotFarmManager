0441fc0d8518f070c0f32560e6c1fd42ff48b4ff
Brandon Pomeroy <pomeroyb@uw.edu>
2015-11-16 20:30:06 -0800

    Add support for importing data
    Add pure.js and accompanying libs. Setup framework for adding content
    based on the data.js

---
+++ frontend/data.js
@@ -0,0 +1,62 @@
+
+var data = {
+   "farm":{
+      "bot000001":{
+         "status":"offline",
+         "problems":{
+            "extruderFanFailure":false,
+            "tornInsulation":false,
+            "wornYCarriage":false,
+            "xEndstopFailure":false,
+            "xStepperFailure":false,
+            "hotEndFailure":false
+         },
+         "type":"Replicator 2X",
+         "name":"Sharon",
+         "events":{
+            "ovalHoles":0,
+            "highResUnderExtrusion":0,
+            "underExtrusion":0,
+            "layerShift":0
+         }
+      },
+      "bot000002":{
+         "status":"online",
+         "problems":{
+            "extruderFanFailure":false,
+            "tornInsulation":false,
+            "wornYCarriage":false,
+            "xEndstopFailure":false,
+            "xStepperFailure":false,
+            "hotEndFailure":false
+         },
+         "type":"Replicator 2X",
+         "name":"Brandon",
+         "events":{
+            "ovalHoles":0,
+            "highResUnderExtrusion":0,
+            "underExtrusion":0,
+            "layerShift":0
+         }
+      },
+      "bot000003":{
+         "status":"online",
+         "problems":{
+            "extruderFanFailure":false,
+            "tornInsulation":false,
+            "wornYCarriage":false,
+            "xEndstopFailure":false,
+            "xStepperFailure":false,
+            "hotEndFailure":true
+         },
+         "type":"Replicator 2X",
+         "name":"Phillip",
+         "events":{
+            "ovalHoles":0,
+            "highResUnderExtrusion":0,
+            "underExtrusion":0,
+            "layerShift":0
+         }
+      }
+   }
+}
